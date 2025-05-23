bf523b7ad165d89479e0a17c10cef69f941ec946
Frouzin <matheusereis44@gmail.com>
2025-05-23 00:06:52 -0300

    tentativa de atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser'; 
 import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, BrowserModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ToDoList';
   tarefa = '';
+
   public items = ['Item 1', 'Item 2', 'Item 3'];
 
   addTarefa() {
