00e51152a64f22a6287ecf9898943633c19a59f7
Frouzin <matheusereis44@gmail.com>
2025-05-22 22:57:18 -0300

    adicionar e remover tarefas feito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ToDoList';
+  tarefa = '';
+  public items = ['Item 1', 'Item 2', 'Item 3'];
+
+  addTarefa() {
+    if (this.tarefa.trim() !== '') {
+      this.items.push(this.tarefa);
+      this.tarefa = '';
+    }
+  }
+
+  removeTarefa(index: number) {
+    this.items.splice(index, 1);
+  }
 }
